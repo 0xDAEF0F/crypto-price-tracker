e5cbc0dbb31cab8b01fdfd02369c927c4e93297d
Alex T <aletapia@proton.me>
2025-03-29 23:05:42 -0600

    (chore): Remove hyperliquid dependency and refactor to use fetch for API calls.

--- index.ts
+++ index.ts
@@ -1,33 +1,45 @@
 #!/usr/bin/env bun
 
-import { Hyperliquid, type CandleSnapshot } from "hyperliquid";
+import { fetch } from "bun";
 
+const HL_BASE_URL = "https://api.hyperliquid.xyz";
 const ONE_DAY_MS = 86400 * 1000;
 
-const hl = new Hyperliquid({
-  enableWs: false,
-});
+async function fetchHlSnapshot(coin: string, interval: string): Promise<any> {
+  const now = Date.now();
+  return fetch(`${HL_BASE_URL}/info`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      type: "candleSnapshot",
+      req: {
+        coin,
+        interval,
+        startTime: now - ONE_DAY_MS,
+        endTime: now,
+      },
+    }),
+  }).then((res) => res.json());
+}
 
-let btcSnapshot: CandleSnapshot;
-let solSnapshot: CandleSnapshot;
-
+let btcSnapshot: any;
+let solSnapshot: any;
 try {
-  const nowMs = Date.now();
-  const yesterdayMs = nowMs - ONE_DAY_MS;
-
   const [btcSnapshot_, solSnapshot_] = await Promise.all([
-    hl.info.getCandleSnapshot("BTC-PERP", "1m", yesterdayMs, nowMs),
-    hl.info.getCandleSnapshot("SOL-PERP", "1m", yesterdayMs, nowMs),
+    fetchHlSnapshot("BTC", "1m"),
+    fetchHlSnapshot("SOL", "1m"),
   ]);
   btcSnapshot = btcSnapshot_;
   solSnapshot = solSnapshot_;
-} catch (e: unknown) {
-  console.error("Error retrieving snapshots", e);
+} catch (error) {
+  console.error("Error retrieving snapshots", error);
   process.exit(1);
 }
 
-btcSnapshot.sort((a, b) => a.t - b.t);
-solSnapshot.sort((a, b) => a.t - b.t);
+btcSnapshot.sort((a: any, b: any) => a.t - b.t);
+solSnapshot.sort((a: any, b: any) => a.t - b.t);
 
 const btcT0ClosingPrice = +btcSnapshot[0]?.c!;
 const btcT1ClosingPrice = +btcSnapshot[btcSnapshot.length - 1]?.c!;
@@ -56,5 +68,4 @@ const data = {
 };
 
 console.log(JSON.stringify(data));
-
 process.exit(0);
